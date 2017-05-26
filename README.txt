4842a44bce885ea1d4ea55de47ee52d2dbaa01d8
Prashanth Kurimella <pkurimella@open-logix.com>
2017-05-25 22:27:50 -0400

    Initial commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/css-tricks/pen/pvamyK.
+
+ Demo for this CSS Tricks tutorial: http://css-tricks.com/seamless-responsive-photo-grid/
---
+++ js/index.js
@@ -0,0 +1,23 @@
+function getRandomSize(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+function getRandomSubject() {
+  var items = ['cars', 'buildings', 'paris', 'detroit', 'phones', 'india'];
+  return items[Math.floor(Math.random() * items.length)];
+}
+function getRandomStatus() {
+  var items = ['fa-check-circle', 'fa-upload', 'fa-exclamation-circle'];
+  return items[Math.floor(Math.random() * items.length)];
+}
+var allImages = "";
+
+for (var i = 0; i < 25; i++) {
+  var width = getRandomSize(400, 1200);
+  var height =getRandomSize(200, 600);
+  var subject = getRandomSubject();
+  allImages += '<div class="imgContainer"><i class="fa fa-trash fa-pull-right"></i>';
+  allImages += '<img class="imgItem" src="http://www.loremflickr.com/' + width + '/' + height + '/' + subject + '">';
+  allImages += '<i class="fa '+getRandomStatus()+' fa-pull-right"></i></div>';
+}
+
+$('#photos').append(allImages);
